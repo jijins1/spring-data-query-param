d742069342095b37407b926aed134c90d2b149ed
Ruokki <leo.gerberon@live.fr>
2021-04-25 23:16:19 +0200

    add javadoc

--- common/src/main/java/com/ruokki/query/annotation/CommonCriteria.java
+++ common/src/main/java/com/ruokki/query/annotation/CommonCriteria.java
@@ -9,10 +9,26 @@ public abstract class CommonCriteria<T> {
         this.fromClass = fromClass;
     }
 
+    /**
+     * Base class returned by your request
+     * <p>
+     * Exemple :
+     *
+     * <code>List<A> result = criteriaResearchRepository.searchByCriteria(ACriteria)</code>
+     * {@link #getFromClass()} on ACriteria return `A.class`
+     *
+     * @return the base class from your request
+     * @implNote this class is setted by the generated code
+     */
     public Class<T> getFromClass() {
         return fromClass;
     }
 
+    /**
+     * Return a map with all setted value in your criteria
+     * @return
+     * @implNote this method is used for creating the request
+     */
     public abstract Map<TypeCriteria, Map<String, ?>> getCriteriaMap();
 
     public enum TypeCriteria {
--- common/src/main/java/com/ruokki/query/annotation/Criteria.java
+++ common/src/main/java/com/ruokki/query/annotation/Criteria.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * Annotation to add on your class for create Criteria Class
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface Criteria {
